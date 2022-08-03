2d93e060c76dc68b8ab789b657410b929b77120d
ecila7290 <jihoon.kim89@gmail.com>
2022-08-03 23:57:08 +0900

    bigger application wip

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import Depends, FastAPI
+
+from .dependencies import get_query_token, get_token_header
+from .internal import admin
+from .routers import items, users
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+app.include_router(items.router)
+app.include_router(users.router)
+app.include_router(admin.router)
